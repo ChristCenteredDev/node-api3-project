b98d7ac041ef435b25f43ff704ea6bc978e39a6a
William M <wmooney1984@gmail.com>
2020-01-23 16:52:44 -0500

    MVP Completed

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const User = require('./userDb.js');
+const Post = require('../posts/postDb.js');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  // do your magic!
+router.post('/', validateUser, (req, res) => {
+  const { name } = req.body;
+  User.insert({ name })
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ errorMessage: 'Error inserting user' });
+    });
 });
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
+  const post = req.body;
+
+  Post.insert(post)
+    .then(post => {
+      res.status(201).json(post);
+    })
+    .catch(err => {
+      res.status(500).json({ errorMessage: 'error adding post' });
+    });
 });
 
 router.get('/', (req, res) => {
@@ -22,52 +39,102 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
-  User.getById(id).then(user => {
-    if (user) {
-      res.status(200).json(user);
-    } else {
-      res.status(404).json({ errorMessage: "User with ID doesn't exist" });
-    }
-  });
+router.get('/:id', validateUserId, (req, res) => {
+  res.status(200).json(req.user);
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
+router.get('/:id/posts', validateUserId, (req, res) => {
+  const { id } = req.user;
+  User.getUserPosts(id)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ errorMessage: 'Error getting user posts' });
+    });
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete('/:id', validateUserId, (req, res) => {
+  const { id } = req.user;
+  User.remove(id)
+    .then(() => res.status(204).end())
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ errorMessage: 'Error deleting user' });
+    });
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validateUserId, (req, res) => {
   const { id } = req.params;
   const { name } = req.body;
 
-  User.getById(id).then(user => {
-    if (user) {
-      User.update(id, { name }).then(updated => {
-        res.status(200).json(updated);
-      });
-    } else {
-      res.status(404).json({ errorMessage: "User with ID doesn't exist" });
-    }
-  });
+  User.update(id, { name })
+    .then(() => {
+      User.getById(id)
+        .then(user => res.status(200).json(user))
+        .catch(err => {
+          console.log(err);
+          res.status(500).json({ errorMessage: 'Error getting user' });
+        });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ errorMessage: 'Error updating user' });
+    });
 });
 
 //custom middleware
 
+function verifyUserID(id, cb) {
+  User.getById(id).then(user => {
+    if (user) {
+      cb();
+    } else {
+      res.status(404).json({ errorMessage: "User with ID doesn't exist" });
+    }
+  });
+}
+
 function validateUserId(req, res, next) {
-  // do your magic!
+  const { id } = req.params;
+  User.getById(id).then(user => {
+    if (user) {
+      req.user = user;
+      next();
+    } else {
+      res.status(404).json({ errorMessage: "User with ID doesn't exist" });
+    }
+  });
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  const { name } = req.body;
+  if (!name) {
+    return res.status(400).json({ errorMessage: 'Name required' });
+  }
+
+  if (typeof name !== 'string') {
+    return res.status(400).json({ errorMessage: 'Name must be a string' });
+  }
+
+  next();
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+  const { id: user_id } = req.params;
+  const { text } = req.body;
+
+  if (!req.body) {
+    return res.status(400).json({ errorMessage: 'Post requires body' });
+  }
+
+  if (!text) {
+    return res.status(400).json({ errorMessage: 'Post requires text' });
+  }
+
+  req.body = { user_id, text };
+  next();
 }
 
 module.exports = router;
