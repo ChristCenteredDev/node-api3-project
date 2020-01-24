6f3907077ad660959972576a840e1f50bb46245f
William M <wmooney1984@gmail.com>
2020-01-23 21:31:07 -0500

    Adjusted port settings for Heroku

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // code away!
 const server = require('./server');
+const port = process.env.PORT || 3000;
 
-server.listen(3000, () => console.log('Server on 3000'));
+server.listen(port, () => console.log(`Server listening on port ${port}`));
